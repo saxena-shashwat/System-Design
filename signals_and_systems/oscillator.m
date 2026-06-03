52c2646d2aa1237bc436339979234aee2651209c
saxena-shashwat <g3shashwat@gmail.com>
2026-06-03 18:38:02 +0530

    comments

--- signals_and_systems/oscillator.m
+++ signals_and_systems/oscillator.m
@@ -1,15 +1,15 @@
 clc, clear
 
-w = pi/4;
+w = 0.01;
 A = [cos(w) -sin(w); sin(w) cos(w)];
 B = [0; 1];
 C = [1 0];
 
-s = zeros(10, 2);
+s = zeros(1000, 2);
 
 s(1, :) = [0 1];
 
-for i=1:9
+for i=1:999
     s(i+1, :) = s(i, :)*A;
 end
 
--- signals_and_systems/unitDelay.m
+++ signals_and_systems/unitDelay.m
@@ -1,7 +1,15 @@
 clc, clear
 
-x = [1 0 0 1 1 1 0 0 1 0 1]';
-s = [0; x];
-y = s;
+x = randi([0 1], 10, 1)
+s = zeros(11, 1)
 
+for i = 1:10
+    s(i+1) = (x(i) == 1);
+end
+
+disp(x);
 disp(s);
+hold on
+scatter(1:1:10, x);
+scatter(1:1:11, s, "filled");
+axis([-0.5 12 -0.5 1.5]);
