3e8b804305fa2601b09a6767d931cb113e7f808a
saxena-shashwat <g3shashwat@gmail.com>
2026-06-04 21:17:33 +0530

    improved basic signals plot

--- signals_and_systems/signals.m
+++ signals_and_systems/signals.m
@@ -1,60 +1,42 @@
-% different signals and how to use them in matlab
+uImplsIn = linspace(0, 10, 1000);
+uImplsOut = (uImplsIn > 4.99) & (uImplsIn < 5.001);
 
-% unit step (@ x = 0)
-x = linspace(-10, 10);
-u = (x>=0);
+expIn = linspace(0, 10, 1000);
+expConst = 0.8;
+expOut = expConst .^ expIn;
+
+sqImplsIn = linspace(0, 10, 1000);
+sqImplsOut = (sqImplsIn > 4) & (sqImplsIn < 5);
+
+rampIn = linspace(0, 10, 1000);
+rampConst = 5;
+rampOut = rampIn;
 
 figure(1)
-plot(x,u);
-title('unit step');
-axis([-10, 10, -1, 1.5]);
+subplot(2, 2, 1);
+plot(uImplsIn, uImplsOut);
+axis([-0.5 10.5 -0.5 1.5]);
+title('Unit Imulse');
+xlabel('Time');
+ylabel('Amplitude');
 
-% unit impulse (@ x = 0)
-x = linspace(-10, 10);
-u = (x == 0);
+subplot(2, 2, 2);
+plot(sqImplsIn, sqImplsOut);
+axis([-0.5 10.5 -0.5 1.5]);
+title('Square Pulse');
+xlabel('Time');
+ylabel('Amplitude');
 
-figure(2)
-plot(x, u);
-title('unit impulse');
-axis([-10 10 -1 1.5]);
+subplot(2, 2, 3);
+plot(rampIn, rampOut);
+axis([-0.5 10.5 -0.5 11]);
+title('Unit Ramp');
+xlabel('Time');
+ylabel('Amplitude');
 
-% ramp (by a factor of a = 5) (@ x = 0)
-x = linspace(-10, 10);
-a = 5;
-u = a .* (x >= 0);
-
-figure(3)
-plot(x, u);
-title('ramp');
-axis([-10 10 -1 6]);
-
-% exponential (a = 0.8)
-x = linspace(0, 20);
-a = 0.8;
-f = a.^x;
-
-figure(4)
-plot(x, f);
-title('exponential');
-axis([0 20 0 1]);
-
-
-
-% square wave
-x = linspace(0, 10, 1000);
-a = double((sin(pi*x)>=0));
-
-figure(5)
-plot(x, a);
-title('square wave');
-axis([0 10 -0.5 1.5]);
-
-
-% square pulse
-x = linspace(0, 10, 1000);
-p = (x>2) & (x<5);
-
-figure(6);
-plot(x, p);
-title('square pulse');
-axis([0 10 -0.5 1.5]);
+subplot(2, 2, 4);
+plot(expIn, expOut);
+axis([-0.5 10.5 -0.5 1.5]);
+title('Exponential Decay');
+xlabel('Time');
+ylabel('Amplitude');
