8e12eb2f2e4631a48c8c2d2b74b2a0995aa5b9b1
saxena-shashwat <g3shashwat@gmail.com>
2026-06-05 13:36:40 +0530

    updated oscillator frequency

--- signals_and_systems/oscillator.m
+++ signals_and_systems/oscillator.m
@@ -1,6 +1,6 @@
 clc, clear
 
-w = 0.01;
+w = pi/60;
 A = [cos(w) -sin(w); sin(w) cos(w)];
 B = [0; 1];
 C = [1 0];
@@ -15,5 +15,5 @@ end
 
 disp(s);
 
-scatter(s(:, 1), s(:, 2))
+scatter(s(:, 1), s(:, 2), 'filled')
 axis([-1.5 1.5 -1.5 1.5]);
