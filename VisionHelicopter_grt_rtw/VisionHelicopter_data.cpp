066838f635eb918a274488b4ee59ac87a62d1fb4
saxena-shashwat <g3shashwat@gmail.com>
2026-07-05 14:41:10 +0530

    esp32

---
+++ VisionHelicopter_grt_rtw/VisionHelicopter_data.cpp
@@ -0,0 +1,31 @@
+/*
+ * VisionHelicopter_data.cpp
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "VisionHelicopter".
+ *
+ * Model version              : 1.10
+ * Simulink Coder version : 25.2 (R2025b) 28-Jul-2025
+ * C++ source code generated on : Sun Jul  5 14:37:35 2026
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "VisionHelicopter.h"
+
+/* Constant parameters (default storage) */
+const ConstP_VisionHelicopter_T VisionHelicopter_ConstP{
+  /* Pooled Parameter (Expression: [0 1; 0 -0.01/0.03125];)
+   * Referenced by:
+   *   '<Root>/Plant (Pitch)'
+   *   '<Root>/Plant (Roll)'
+   */
+  { 1.0, -0.32 }
+};
---
+++ VisionHelicopter_grt_rtw/VisionHelicopter_types.h
@@ -0,0 +1,34 @@
+/*
+ * VisionHelicopter_types.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "VisionHelicopter".
+ *
+ * Model version              : 1.10
+ * Simulink Coder version : 25.2 (R2025b) 28-Jul-2025
+ * C++ source code generated on : Sun Jul  5 14:37:35 2026
+ *
+ * Target selection: grt.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef VisionHelicopter_types_h_
+#define VisionHelicopter_types_h_
+#ifndef SS_UINT64
+#define SS_UINT64                      17
+#endif
+
+#ifndef SS_INT64
+#define SS_INT64                       18
+#endif
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_VisionHelicopter_T RT_MODEL_VisionHelicopter_T;
+
+#endif                                 /* VisionHelicopter_types_h_ */
