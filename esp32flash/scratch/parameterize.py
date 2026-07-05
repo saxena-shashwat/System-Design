77c45cb0e24b6a824caa6bb83cd1908fd712e465
naman <notnamansinha@gmail.com>
2026-07-05 17:29:45 +0530

    Set Vx target to 1.0 m/s

--- esp32flash/scratch/parameterize.py
+++ esp32flash/scratch/parameterize.py
@@ -66,7 +66,7 @@ struct_def = """
     real_T Filter_Coef = 100.0;
     real_T Alt_Filter_Coef = 2.0;
     real_T Alt_LowPass_Input = 5.0; // For 5m alt setpoint, DC gain = 10/2 = 5
-    real_T Vx_LowPass_Input = 0.0;  // Exposed Vx reference input
+    real_T Vx_LowPass_Input = 1.0;  // Exposed Vx reference input
     real_T Thrust_Transfer_Gain = -10000.0;
     real_T Gravity = 9.81;
     real_T Thrust_Base_Gain = 0.75;
