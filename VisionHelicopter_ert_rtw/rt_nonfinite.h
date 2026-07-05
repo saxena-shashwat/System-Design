5beee3f7e36efe6455d25df6510b9eff7c7f79f6
saxena-shashwat <g3shashwat@gmail.com>
2026-07-05 15:01:47 +0530

    ill organise later

---
+++ VisionHelicopter_ert_rtw/rt_nonfinite.h
@@ -0,0 +1,51 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: rt_nonfinite.h
+//
+// Code generated for Simulink model 'VisionHelicopter'.
+//
+// Model version                  : 1.10
+// Simulink Coder version         : 25.2 (R2025b) 28-Jul-2025
+// C/C++ source code generated on : Sun Jul  5 14:59:06 2026
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Intel->x86-64 (Windows64)
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#ifndef rt_nonfinite_h_
+#define rt_nonfinite_h_
+#include "rtwtypes.h"
+#ifdef __cplusplus
+
+extern "C"
+{
+
+#endif
+
+  extern real_T rtInf;
+  extern real_T rtMinusInf;
+  extern real_T rtNaN;
+  extern real32_T rtInfF;
+  extern real32_T rtMinusInfF;
+  extern real32_T rtNaNF;
+  extern boolean_T rtIsInf(real_T value);
+  extern boolean_T rtIsInfF(real32_T value);
+  extern boolean_T rtIsNaN(real_T value);
+  extern boolean_T rtIsNaNF(real32_T value);
+
+#ifdef __cplusplus
+
+}                                      // extern "C"
+
+#endif
+#endif                                 // rt_nonfinite_h_
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
